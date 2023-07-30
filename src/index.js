bd2d7c933b02d667c6ca536737881831416f27c3
Mhee <subhibhat@users.noreply.github.com>
2023-07-30 22:58:14 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,45 +1,53 @@
-(() => {
-	const list = []
-	const tbody= document.querySelector('tbody')
-	
-	const gradeToPoint = grade => {
-		switch (grade) {
-			case 'A': return 4; case 'B+': return 3.5;
-			case 'B': return 3; case 'C+': return 2.5;
-			case 'C': return 2; case 'D+': return 1.5;
-			case 'D': return 1; default: return 0;
-		}
-	}
-	
-	for (var i = 0, row; row = tbody.rows[i]; i++) {
-		let data = {
-			code: row.cells[2].innerHTML,
-			unit: parseInt(row.cells[3].innerHTML),
-			point: gradeToPoint(row.cells[4].innerHTML.trim())
-		}
+const calculateGPAX = () => {
+    const gradeToPoint = (grade) => {
+        switch (grade) {
+            case 'A': return 4; case 'B+': return 3.5;
+            case 'B': return 3; case 'C+': return 2.5;
+            case 'C': return 2; case 'D+': return 1.5;
+            case 'D': return 1; default: return 0;
+        }
+    };
 
-		let index = list.findIndex(item => item.code == data.code)
+    const getTableRowData = (row) => {
+        const code = row.cells[2].innerHTML;
+        const unit = parseInt(row.cells[3].innerHTML);
+        const point = gradeToPoint(row.cells[4].innerHTML.trim());
+        return { code, unit, point };
+    };
 
-		if (index > -1) {
-			let oldData = list[index]
-			list[index] = oldData.point > data.point ? oldData : data
-		}
+    const tbody = document.querySelector('tbody');
+    const list = [];
 
-		list.push(data)
-	}
+    for (const row of tbody.rows) {
+        const data = getTableRowData(row);
 
-	const point= list.map(item => item.unit * item.point).reduce((prv, cur) => prv + cur)
-	const unit = list.map(item => item.unit).reduce((prv, cur) => prv + cur)
-	const gpax = parseFloat(point/unit).toFixed(2)
+        const index = list.findIndex(item => item.code === data.code);
+        if (index > -1) {
+            const oldData = list[index];
+            list[index] = oldData.point > data.point ? oldData : data;
+        } else {
+            list.push(data);
+        }
+    }
 
-	const show = document.getElementById('yw0')
-	
-	show.innerHTML = ''
-	show.classList.add('alert', 'alert-success')
-	show.setAttribute('style', 'margin-top: 15px')
+    const point = list.map(item => item.unit * item.point).reduce((prev, cur) => prev + cur);
+    const unit = list.map(item => item.unit).reduce((prev, cur) => prev + cur);
+    const gpax = parseFloat(point / unit).toFixed(2);
 
-	show.innerHTML = `
-		หน่วยกิตรวม: <strong>${unit}</strong><br />
-		เกรดเฉลี่ย: <strong>${gpax}</strong>
-	`
-})()
+    return { unit, gpax };
+};
+
+const showGPAX = () => {
+    const { unit, gpax } = calculateGPAX();
+    const show = document.getElementById('yw0');
+
+    show.innerHTML = `
+        หน่วยกิตรวม: <strong>${unit}</strong><br />
+        เกรดเฉลี่ย: <strong>${gpax}</strong>
+    `;
+
+    show.classList.add('alert', 'alert-success');
+    show.setAttribute('style', 'margin-top: 15px');
+};
+
+document.addEventListener('DOMContentLoaded', showGPAX);
